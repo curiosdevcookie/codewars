bfc1350eb52e2c530bdda910fd1ba04a389f6f08
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2022-10-19 19:06:49 +0200

    Add kata 'Stop gninnipS My sdroW!' in Javascript

--- spinWords/spinWords.js
+++ spinWords/spinWords.js
@@ -1,5 +1,4 @@
-//https://www.codewars.com/kata/5264d2b162488dc400000001/train/javascript
-
+//https://www.codewars.com/kata/5264d2b162488dc400000001/javascript
 /*Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 
 Examples:
@@ -9,8 +8,28 @@ spinWords( "This is a test") => returns "This is a test"
 spinWords( "This is another test" )=> returns "This is rehtona test"
 */
 
-
+const string = 'Welcome to the jungle';
+console.log(string);
 
 function spinWords(string) {
-  //TODO Have fun :)
+
+  //Split the string into an array of words:
+  const words = string.split(' ');
+
+  //Take words with 5 or more letters,
+  const reversedWords = words.map(word => {
+    return ((word.length >= 5) ?
+      // reverse the letters of those words and join them back together,
+      word.split('').reverse().join('')
+      // return words that are less than five letters as is.
+      : word);
+  });
+  //stick them back together into a string and return from the function.  
+
+  const stringReversed = reversedWords.join(' ');
+  return stringReversed;
 }
+
+spinWords(string);
+
+console.log(spinWords(string));
