d8d39a2fb3f1a3b6b2327a9fe0ac7bb3c0cd8da6
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2023-03-09 19:53:04 +0100

    Optimize solution to trolls exercise.

--- trolls/trolls.js
+++ trolls/trolls.js
@@ -1,17 +1,11 @@
 
-//
+//https://www.codewars.com/kata/52fba66badcd10859f00097e/train/javascript
 
 const ourString = "This website is for losers LOL!"
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 
 function disemvowel(str) {
-  const newStr = str.split('').map(letter => {
-    if (vowels.includes(letter.toLowerCase())) {
-      return '';
-    } else {
-      return letter;
-    }
-  });
+  const newStr = str.split('').filter((letter) => !vowels.includes(letter.toLowerCase()));
 
   str = newStr.join('');
 
