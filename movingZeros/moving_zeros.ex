e6bbb7726fabf1a0b61952eb64d09df05d1cc9aa
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2023-03-31 11:23:03 +0200

    Remove katas.

--- movingZeros/moving_zeros.ex
+++ movingZeros/moving_zeros.ex
@@ -3,8 +3,8 @@
 # moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 
 defmodule MovingZeros do
-  def moving_zeros (arr)do
-    arr = [false,1,0,1,2,0,1,3,"a"];
-    Enum.filter(arr, fn x -> x !== 0 end) ++ Enum.filter(arr, fn x -> x === 0 end)
+  def moving_zeros (list)do
+    list = [0,0,"test", :atom, 0, 9, 0,];
+    Enum.filter(list, fn x -> x !== 0 end) ++ Enum.filter(list, fn x -> x === 0 end)
   end
 end
